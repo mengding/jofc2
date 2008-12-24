e150bf71f235f25b50ab7357d73cf2ad56995006
mhaufler <mhaufler@b07dabbe-d10c-11dd-a9c2-f1de684657c9>
2008-12-24 08:24:17 +0000

    Renamed Packets to JOFC2 Also refactored licence text

---
+++ src/main/java/jofc2/model/Chart.java
@@ -0,0 +1,166 @@
+/*
+This file is part of JOFC2.
+
+JOFC2 is free software: you can redistribute it and/or modify
+it under the terms of the Lesser GNU General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+JOFC2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+See <http://www.gnu.org/licenses/lgpl-3.0.txt>.
+ */
+package jofc2.model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+
+import jofc2.OFC;
+import jofc2.OFCException;
+import jofc2.model.axis.XAxis;
+import jofc2.model.axis.YAxis;
+import jofc2.model.elements.Element;
+
+
+/**
+ * This is the most important class in the Java OFC library. Start here,
+ * configuring the title, axes, legends, labels, and draw-able elements in your
+ * chart. Coerce the object to a String with the toString() method to get the
+ * chart data back out.
+ */
+public class Chart implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1868082240169089976L;
+	private Text title;
+	private XAxis x_axis;
+	private YAxis y_axis;
+	private YAxis y_axis_right;
+	private Text y_legend;
+	private Text x_legend;
+	private String bg_colour;
+	private Collection<Element> elements = new ArrayList<Element>();
+
+	public XAxis getXAxis() {
+		return x_axis;
+	}
+
+	public Chart() {
+	//nothing...
+	}
+
+	public Chart(String titleText) {
+		this(titleText, null);
+	}
+
+	public Chart(String titleText, String style) {
+		this.setTitle(new Text(titleText, style));
+	}
+
+	public Chart setXAxis(XAxis x_axis) {
+		this.x_axis = x_axis;
+		return this;
+	}
+
+	public YAxis getYAxis() {
+		return y_axis;
+	}
+
+	public Chart setYAxis(YAxis y_axis) {
+		this.y_axis = y_axis;
+		return this;
+	}
+
+	public Chart setYAxisRight(YAxis y_axis_right) {
+		this.y_axis_right = y_axis_right;
+		return this;
+	}
+
+	public YAxis getYAxisRight() {
+		return y_axis_right;
+	}
+
+	public Text getTitle() {
+		return title;
+	}
+
+	public Chart setTitle(Text title) {
+		this.title = title;
+		return this;
+	}
+
+	public Text getXLegend() {
+		return x_legend;
+	}
+
+	public Chart setXLegend(Text x_legend) {
+		this.x_legend = x_legend;
+		return this;
+	}
+
+	public Text getYLegend() {
+		return y_legend;
+	}
+
+	public Chart setYLegend(Text y_legend) {
+		this.y_legend = y_legend;
+		return this;
+	}
+
+	public String getBackgroundColour() {
+		return bg_colour;
+	}
+
+	public Chart setBackgroundColour(String bg_colour) {
+		this.bg_colour = bg_colour;
+		return this;
+	}
+
+	public Collection<Element> getElements() {
+		return elements;
+	}
+
+	public Chart setElements(Collection<Element> elements) {
+		this.elements.clear();
+		this.elements.addAll(elements);
+		return this;
+	}
+
+	public Chart addElements(Element... e) {
+		elements.addAll(Arrays.asList(e));
+		return this;
+	}
+
+	public Chart addElements(Collection<Element> coll) {
+		elements.addAll(coll);
+		return this;
+	}
+
+	public boolean removeElement(Element e) {
+		return elements.remove(e);
+	}
+
+	public Element getElementByText(String text) {
+		for (Element e : getElements()) {
+			if (text.equals(e.getText())) return e;
+		}
+		return null;
+	}
+
+	@Override
+	/*
+	 * @throws OFCException can throw an OFCException if there is a problem
+	 * rendering this Chart object. This exception would indicate an issue with
+	 * the JOFC2 library itself.
+	 */
+	public String toString() throws OFCException {
+		return OFC.getInstance().render(this);
+	}
+}
---
+++ src/main/java/jofc2/model/elements/Element.java
@@ -0,0 +1,97 @@
+/*
+This file is part of JOFC2.
+
+JOFC2 is free software: you can redistribute it and/or modify
+it under the terms of the Lesser GNU General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+JOFC2 is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+See <http://www.gnu.org/licenses/lgpl-3.0.txt>.
+*/
+
+package jofc2.model.elements;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import jofc2.model.metadata.Alias;
+
+
+public abstract class Element implements Serializable{
+    /**
+	 * 
+	 */
+	private static final long serialVersionUID = 3975314200083173622L;
+	private final String type;
+    private Float alpha;
+    private String text;
+    @Alias("font-size") private Integer fontSize;
+    @Alias("tip") private String tooltip;
+    @Alias("gradient-fill") private Boolean gradientFill;
+    private List<Object> values = new ArrayList<Object>();
+    
+    protected Element(String type) {
+        this.type = type;
+    }
+    
+    public String getType() {
+        return type;
+    }
+
+    public Float getAlpha() {
+        return alpha;
+    }
+
+    public Element setAlpha(Float alpha) {
+        this.alpha = alpha;
+        return this;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public Element setText(String text) {
+        this.text = text;
+        return this;
+    }
+
+    public Integer getFontSize() {
+        return fontSize;
+    }
+
+    public Element setFontSize(Integer fontSize) {
+        this.fontSize = fontSize;
+        return this;
+    }
+    
+    public List<Object> getValues() {
+        return values;
+    }
+    
+    public Element setTooltip(String tooltip) {
+        this.tooltip = tooltip;
+        return this;
+    }
+    
+    public String getTooltip() {
+        return tooltip;
+    }
+
+	
+	public Boolean getGradientFill() {
+		return gradientFill;
+	}
+
+	
+	public void setGradientFill(Boolean gradientFill) {
+		this.gradientFill = gradientFill;
+	}
+    
+}
